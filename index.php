a5db0e0249a2b7570c6c49cbfc9398ccf623f520
박유진 <pkeugine@gmail.com>
2020-09-14 12:28:35 +0000

    domcrawler practice

--- index.php
+++ index.php
@@ -15,8 +15,14 @@ $html = ''.$res->getBody();
 // get the data from $url
 $crawler = new Crawler($html);
 
-$nodeValues = $crawler->filter('a')->each(function (Crawler $node, $i) {
-	return $node->text();
+// loop through the data
+$nodeValues = $crawler->filter('.board-text > a')->each(function (Crawler $node, $i) {
+	return [
+		'category' => $node->filter('.category'),
+		'text' => $node->text(),
+		'link' => 'https://www.kw.ac.kr'.$node->attr('href')
+	];
+	//return $node->text();
 });
 
 print_r($nodeValues);
